5c209ea6dd613dac597afbd6611bb44d0c59b345
AliObaji <ali-obaji@hotmail.com>
2017-01-29 21:19:00 +0200

    Scraped first page and converted the items into json. Still need to remove duplicates.

--- server/web-Scrapper/scrapper.py
+++ server/web-Scrapper/scrapper.py
@@ -1,8 +1,35 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import json
 
 wiki = "https://www-banner.aub.edu.lb/catalog/schd_A.htm"
 
 page = urllib.request.urlopen(wiki)
-soup = BeautifulSoup(page,"html.parser")
-print(soup.prettify())
+soup = BeautifulSoup(page, "html.parser")
+all_tables = soup.find_all('td')
+
+data = {}
+data["courses"] = []
+
+currCourse = {"name": "",
+              "number": "",
+              "title": ""
+              }
+count = 0
+for td in all_tables[39:]:
+    if count == 2:
+        currCourse['name'] = td.string
+    elif count == 3:
+        currCourse['number'] = td.string
+    elif count == 5:
+        currCourse['title'] = td.string
+
+    if count == 34:
+        data["courses"].append(currCourse)
+        currCourse = {"name": ""}
+        count = 0
+    else:
+        count += 1
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
